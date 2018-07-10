355eb3f2903e56820bdbac7095233f5507d6a6ff
Igor Drozdov <ihardrozdov@gmail.com>
2018-07-11 00:43:34 +0300

    Add Elm to the project

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "vendor", "elm"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -47,7 +47,13 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+    elmBrunch: {
+      elmFolder: "elm",
+      mainModules: ["src/Leader.elm"],
+      makeParameters: ["--warn"],
+      outputFolder: '../js/',
+    },
   },
 
   modules: {
--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,11 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Elm from './leader';
+
+const elmDiv = document.querySelector('#elm_target');
+
+if (elmDiv) {
+  Elm.Leader.embed(elmDiv, { gameId: elmDiv.dataset.gameId });
+}
