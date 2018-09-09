326bd39090072fc1b97d461f6fe5b23e69846214
Igor Drozdov <ihardrozdov@gmail.com>
2018-09-09 13:10:36 +0200

    Use current process in the Followers.CurrentChannel

--- lib/playground_web/channels/followers/current_channel.ex
+++ lib/playground_web/channels/followers/current_channel.ex
@@ -4,15 +4,25 @@ defmodule PlaygroundWeb.Followers.CurrentChannel do
   alias Playground.{Mafia, Repo}
 
   def join("followers:current:" <> ids, _payload, socket) do
-    [game_id, _player_id] = String.split(ids, ":")
-    game = Mafia.get_game!(game_id) |> Repo.preload(:players)
+    [game_uuid, player_uuid] = String.split(ids, ":")
+    game = Mafia.get_game!(game_uuid) |> Repo.preload(:players)
 
-    {:ok, game, assign(socket, :game, game)}
+    new_socket =
+      socket
+      |> assign(:game_uuid, game_uuid)
+      |> assign(:player_uuid, player_uuid)
+
+    {:ok, game, new_socket}
   end
 
-  def handle_in("choose_candidate", %{"player_id" => player_id}, socket) do
-    [{pid, _}] = Registry.lookup(Playground.Mafia.Registry, {:current, socket.assigns.game.id})
-    GenServer.cast(pid, {:choose_candidate, player_id})
+  def handle_in("choose_candidate", %{"player_id" => target_player_uuid}, socket) do
+    %{game_uuid: game_uuid, player_uuid: current_player_uuid} = socket.assigns
+    current_process = Playground.Mafia.Narrator.current(game_uuid)
+
+    require Logger
+    Logger.info(inspect(current_process))
+
+    GenServer.cast(current_process, {:choose_candidate, target_player_uuid, current_player_uuid})
 
     {:noreply, socket}
   end
