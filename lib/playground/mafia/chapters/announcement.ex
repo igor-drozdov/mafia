9cb12fd86a81fa32e9973241a394dc2d4cc7b9c4
Igor Drozdov <ihardrozdov@gmail.com>
2018-09-09 13:13:09 +0200

    Implement Announcement chapter

--- lib/playground/mafia/chapters/announcement.ex
+++ lib/playground/mafia/chapters/announcement.ex
@@ -1,6 +1,33 @@
 defmodule Playground.Mafia.Chapters.Announcement do
   use Playground.Mafia.Chapter
 
-  defp handle_run(game_uuid) do
+  alias Playground.Mafia.{PlayerStatus, PlayerRound, Player, Chapters.RoundEnds}
+  alias Playground.Repo
+  alias PlaygroundWeb.Endpoint
+
+  import Ecto.Query
+
+  defp handle_run(%{game_uuid: game_uuid, round_id: round_id, players: players} = state) do
+    player = ostracize_deprecated_player(round_id)
+    notify_leader(game_uuid, player)
+
+    RoundEnds.run(game_uuid, Map.put(state, :players, players -- [player]))
+
+    {:stop, :shutdown, state}
+  end
+
+  def ostracize_deprecated_player(round_id) do
+    most_deprecated_player =
+      Player.by_status(round_id, :deprecated)
+      |> group_by([p, pr, ps], [ps.player_round_id, p.id])
+      |> order_by([p, pr, ps], desc: count(ps.id))
+      |> limit(1)
+      |> Repo.one()
+
+    PlayerRound.create_status(round_id, most_deprecated_player.id, :ostracized)
+  end
+
+  def notify_leader(game_uuid, player) do
+    Endpoint.broadcast("leader:current:#{game_uuid}", "city_wakes", %{players: [player]})
   end
 end
---
+++ test/playground/mafia/chapters/announcement_test.exs
@@ -0,0 +1,47 @@
+defmodule Playground.Mafia.Chapters.AnnouncementTest do
+  use PlaygroundWeb.ChannelCase
+
+  alias Playground.Mafia.Chapters.Announcement
+  alias Playground.Mafia.Chapters.RoundBegins
+  alias Playground.Mafia.{PlayerRound, Player}
+  alias Playground.Repo
+
+  import Playground.Factory
+
+  setup do
+    game = insert(:game)
+
+    {:ok, game: game}
+  end
+
+  describe "#notify_leader" do
+    test "broadcast city wakes", %{game: game} do
+      @endpoint.subscribe("leader:current:#{game.id}")
+      player = insert(:player, game: game)
+
+      Announcement.notify_leader(game.id, player)
+
+      assert_broadcast("city_wakes", %{players: [player]})
+    end
+  end
+
+  describe "#ostracize_deprecated_player" do
+    test "broadcast city wakes", %{game: game} do
+      player = insert(:player, game: game)
+      another_player = insert(:player, game: game)
+
+      round = RoundBegins.create_round(game.id, Repo.preload(game, :players).players)
+
+      PlayerRound.create_status(round.id, player.id, :deprecated)
+      PlayerRound.create_status(round.id, player.id, :deprecated)
+      PlayerRound.create_status(round.id, another_player.id, :deprecated)
+
+      Announcement.ostracize_deprecated_player(round.id)
+
+      ostracized_players =
+        Player.by_status(round.id, :ostracized) |> Repo.all() |> Repo.preload(:game)
+
+      assert ostracized_players == [player]
+    end
+  end
+end
