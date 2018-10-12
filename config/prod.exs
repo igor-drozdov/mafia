0e72a3f7cbf949e856c5c6fc2c8ef9852b9ca96f
Igor Drozdov <ihardrozdov@gmail.com>
2018-10-13 02:28:03 +0300

    Add ecto tasks to the edeliver config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mafia, MafiaWeb.Endpoint,
   http: [port: 8888],
-  url: [host: "example.com", port: 80],
+  url: [host: "mafia-game.gq", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
